65b8715066605097e216cc8fde54e5b40a2cebbd
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-11 14:16:19 -0800

    updated function as per exercise instruction

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,9 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors() {}
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function (err, res, body) {
+    cb(err, body);
+  });
+}
